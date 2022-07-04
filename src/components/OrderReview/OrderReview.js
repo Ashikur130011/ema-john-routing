f16eca07e616b50ccf72e9920aad3a2fa1dd010d
Ashikur Rahman <smashikurrahman24@gmail.com>
2022-07-04 15:10:18 +0600

    updated

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useCart from '../../hooks/useCart/useCart';
 import useProducts from '../../hooks/useProducts/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import OrderedProduct from '../OrderedProduct/OrderedProduct';
 
@@ -10,6 +11,7 @@ const OrderReview = () => {
     const removeProduct = (key) =>{
         const restProduct = cart.filter(product=> product.key !== key)
         setCart(restProduct)
+        removeFromDb(key)
     }
     return (
         <div className='shop-container'>
